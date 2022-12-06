e34087627d556229cbb56f4d30971d5957bade85
Boolatch <108538493+Boolatch@users.noreply.github.com>
2022-12-07 01:52:19 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -18,18 +18,18 @@ export function is_digit(str) {
     }
     else {
       if (str[i] == "0" || 
-          str[i] == "1" ||
-          str[i] == "2" ||
-          str[i] == "3" ||
-          str[i] == "4" ||
-          str[i] == "5" ||
-          str[i] == "6" ||
-          str[i] == "7" ||
-          str[i] == "8" ||
-          str[i] == "9" ) {
+        str[i] == "1" ||
+        str[i] == "2" ||
+        str[i] == "3" ||
+        str[i] == "4" ||
+        str[i] == "5" ||
+        str[i] == "6" ||
+        str[i] == "7" ||
+        str[i] == "8" ||
+        str[i] == "9" ) {
         isDecimal = true;
       } else {
-        isDecimal = false;
+      return false;
       }
     }
   }
